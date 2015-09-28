19d7964cbea7a9035ad872424a45fc794ca55917
nikollaf <franc.nikolla@gmail.com>
2015-09-28 13:51:36 -0500

    navbar-mobile-fix

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -61,41 +61,41 @@
 		</div>
 	</nav>
 
-    <div class="container">
-        <!-- Second navbar for search -->
-        <nav class="tag-nav navbar">
-          <div class="container">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse-3">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-              </button>
-            </div>
-        
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="navbar-collapse-3">
-              <ul class="nav navbar-nav">
-                @foreach ($categories as $category)
-                    <li><a class="tags-a" style="border-color: {{$category->color}}; color: {{$category->color}}" 
-                    href="/tag/{{$category->label}}">{{$category->label}}</a></li>
-                @endforeach
-                
-              </ul>
-              <div class="collapse nav navbar-nav nav-collapse slide-down" id="nav-collapse3">
-                <form class="navbar-form navbar-right" role="search">
-                  <div class="form-group">
-                    <input type="text" class="form-control" placeholder="Search" />
-                  </div>
-                  <button type="submit" class="btn btn-danger"><span class="glyphicon glyphicon-search" aria-hidden="true"></span></button>
-                </form>
+  
+    <!-- Second navbar for search -->
+    <nav class="tag-nav navbar">
+      <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse-3">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+        </div>
+    
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="navbar-collapse-3">
+          <ul class="nav navbar-nav">
+            @foreach ($categories as $category)
+                <li><a class="tags-a" style="border-color: {{$category->color}}; color: {{$category->color}}" 
+                href="/tag/{{$category->label}}">{{$category->label}}</a></li>
+            @endforeach
+            
+          </ul>
+          <div class="collapse nav navbar-nav nav-collapse slide-down" id="nav-collapse3">
+            <form class="navbar-form navbar-right" role="search">
+              <div class="form-group">
+                <input type="text" class="form-control" placeholder="Search" />
               </div>
-            </div><!-- /.navbar-collapse -->
-          </div><!-- /.container -->
-        </nav><!-- /.navbar -->
-    </div>
+              <button type="submit" class="btn btn-danger"><span class="glyphicon glyphicon-search" aria-hidden="true"></span></button>
+            </form>
+          </div>
+        </div><!-- /.navbar-collapse -->
+      </div><!-- /.container -->
+    </nav><!-- /.navbar -->
+
 
     @if (Session::get('success'))
     <div class="alert alert-success alert-block text-center">
